f3550f6cdbcd9d7170e4569883ee3073c4d175d7
riadaman <riadmohammod2000@gmail.com>
2026-04-16 15:53:30 +0600

    define relationship between project and deployment chexk model

--- app/Models/DeploymentChecks.php
+++ app/Models/DeploymentChecks.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Projects;
 class DeploymentChecks extends Model
 {
-    //
+     protected $fillable = [
+        'project_id',
+        'title',
+        'is_completed'
+
+    ];
+      public function project(){
+        return $this->hasMany(Project::class);
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\DeploymentChecks;
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'owner_email'
+    ];
+      public function deploymentCheck(){
+        return $this->hasMany(DeploymentChecks::class);
+    }
 }
